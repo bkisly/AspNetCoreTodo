956319d0f37155e65aaea940600139e891f9add9
bkisly <b.kisly@gmail.com>
2022-08-10 17:23:05 +0200

    Dodaj pliki projektów.

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+            TodoViewModel viewModel = new() { Items =  items};
+            return View(viewModel);
+        }
+    }
+}
---
+++ AspNetCoreTodo/Services/ITodoItemService.cs
@@ -0,0 +1,9 @@
+﻿using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemsAsync();
+    }
+}
