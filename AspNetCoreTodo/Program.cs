6d880044d1ebd024fd3ed0bf81c1a98df2ac22f7
bkisly <b.kisly@gmail.com>
2022-08-31 01:36:17 +0200

    Added test admin account

--- AspNetCoreTodo/Program.cs
+++ AspNetCoreTodo/Program.cs
@@ -1,56 +1,64 @@
 using AspNetCoreTodo.Data;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using AspNetCoreTodo.Services;
+using Microsoft.Extensions.DependencyInjection;
+using AspNetCoreTodo;
 
-namespace AspNetCoreTodo;
+var builder = WebApplication.CreateBuilder(args);
 
-public static class Program
+// TODO: add seeding the database with test admin account here
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ITodoItemService, TodoItemService>();
+
+var app = builder.Build();
+
+using(var scope = app.Services.CreateScope())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
 
-        // TODO: add seeding the database with test admin account here
-
-        // Add services to the container.
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(connectionString));
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            .AddEntityFrameworkStores<ApplicationDbContext>();
-        builder.Services.AddControllersWithViews();
-        builder.Services.AddScoped<ITodoItemService, TodoItemService>();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseMigrationsEndPoint();
-        }
-        else
-        {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-        app.MapRazorPages();
-
-        app.Run();
-    }
+    var testUserPw = builder.Configuration.GetValue<string>("SeedUserPW");
+    await SeedData.InitializeAsync(services, testUserPw);
 }
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
--- AspNetCoreTodo/SeedData.cs
+++ AspNetCoreTodo/SeedData.cs
@@ -1,42 +1,55 @@
 ﻿using AspNetCoreTodo.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+using AspNetCoreTodo.Data;
 
 namespace AspNetCoreTodo
 {
     public static class SeedData
     {
-        public static async Task InitializeAsync(IServiceProvider services)
+        public static async Task InitializeAsync(IServiceProvider serviceProvider, string testUserPw)
         {
-            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-            await EnsureRolesAsync(roleManager);
-
-            var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
-            await EnsureTestAdminAsync(userManager);
+            using var context = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>());
+            var adminId = await EnsureUser(serviceProvider, testUserPw, "admin@todo.local");
+            await EnsureRole(serviceProvider, adminId, RoleName.Administrator);
         }
 
-        private static async Task EnsureRolesAsync(RoleManager<IdentityRole> roleManager)
+        private static async Task<string> EnsureUser(IServiceProvider serviceProvider, string testUserPw, string testUserName)
         {
-            if (!await roleManager.RoleExistsAsync(RoleName.Administrator))
-                await roleManager.CreateAsync(new IdentityRole(RoleName.Administrator));
-        }
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var user = await userManager.FindByNameAsync(testUserName);
 
-        private static async Task EnsureTestAdminAsync(UserManager<IdentityUser> userManager)
-        {
-            var testAdmin = await userManager.Users.SingleOrDefaultAsync(user => user.UserName == "admin@todo.local");
-
-            if(testAdmin == null)
+            if(user == null)
             {
-                testAdmin = new IdentityUser
+                user = new IdentityUser
                 {
-                    UserName = "admin@todo.local",
-                    Email = "admin@todo.local"
+                    UserName = testUserName,
+                    EmailConfirmed = true
                 };
 
-                await userManager.CreateAsync(testAdmin, "Te$t123");
-                await userManager.AddToRoleAsync(testAdmin, RoleName.Administrator);
+                await userManager.CreateAsync(user, testUserPw);
             }
+
+            if (user == null) throw new Exception("The password is not strong enough.");
+
+            return user.Id;
+        }
+
+        private static async Task<IdentityResult> EnsureRole(IServiceProvider serviceProvider, string testUserId, string roleName)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            if (roleManager == null) throw new NullReferenceException("RoleManager is null.");
+
+            if (!await roleManager.RoleExistsAsync(roleName))
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var user = await userManager.FindByIdAsync(testUserId);
+
+            if (userManager == null) throw new NullReferenceException("UserManager is null.");
+            if (user == null) throw new Exception("The password is not strong enough.");
+
+            return await userManager.AddToRoleAsync(user, roleName);
         }
     }
 }
