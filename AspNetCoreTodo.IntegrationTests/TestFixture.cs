8d1b2a17d3501a365a98234256306b704fb061ec
bkisly <b.kisly@gmail.com>
2022-09-04 12:10:39 +0200

    Created integration tests

---
+++ AspNetCoreTodo.IntegrationTests/TestFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+
+namespace AspNetCoreTodo.IntegrationTests
+{
+    public class TestFixture : IDisposable
+    {
+        private readonly TestServer _server;
+        public HttpClient Client { get; }
+
+        public TestFixture()
+        {
+            var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureAppConfiguration((context, config) =>
+                {
+                    config.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\..\\AspNetCoreTodo"));
+                    config.AddJsonFile("appsettings.json");
+                });
+            });
+
+            _server = application.Server;
+
+            Client = _server.CreateClient();
+            Client.BaseAddress = new Uri("https://localhost:7245");
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            _server.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ AspNetCoreTodo.IntegrationTests/TodoRouteShould.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.Net;
+
+namespace AspNetCoreTodo.IntegrationTests
+{
+    public class TodoRouteShould : IClassFixture<TestFixture>
+    {
+        private readonly HttpClient _client;
+
+        public TodoRouteShould(TestFixture fixture)
+        {
+            _client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task ChallengeAnnonymousUser()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "/todo");
+            var response = await _client.SendAsync(request);
+
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+
+            if(response.Headers.Location is not null)
+                Assert.Equal("https://localhost:7245/Identity/Account/Login?ReturnUrl=%2Ftodo", response.Headers.Location.ToString());
+        }
+    }
+}
